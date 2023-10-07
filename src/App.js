f3bce24e916b82a3977457dbbde149253e762ed1
riczudaniel <you@example.com>
2023-10-07 21:51:21 +0200

    Add Button to ProdList

--- src/App.js
+++ src/App.js
@@ -3,40 +3,12 @@ import ProductList from './Components/ProductList';
 
 
 function App() {
-  const initialProducts = [
-    {
-      name: 'Eggs',
-      daysUntil: '8 days',
-      productGroup: 'Animal Prod.',
-    },
-    {
-      name: 'Greek Yoghurt',
-      daysUntil: '3 days',
-      productGroup: 'Dairy',
-    },
-  ];
-
- 
-  const [products, setProducts] = useState(initialProducts);
-
-
-  function newProduct() {
-    const newProduct = {
-      name: 'New Product',
-      daysUntil: '7 days',
-      productGroup: 'Group A',
-    };
-
-
-    setProducts([...products, newProduct]);
-  }
-
   return (
     <div>
       <h1 className="text-3xl">Freshly</h1>
       <p className='font-bold underline'>Welcome ddddddaaiel!</p>
-      <button onClick={newProduct}>Add Product</button>
-      <ProductList products={products} addProduct={newProduct}/>
+      
+      <ProductList/>
     </div>
   );
 }
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Product from './Product';
 
 
-const ProductList = ({ products }) => {
+
+const ProductList = () => {
+  const initialProducts = [
+    {
+      name: 'Eggs',
+      daysUntil: '8 days',
+      productGroup: 'Animal Prod.',
+    },
+    {
+      name: 'Greek Yoghurt',
+      daysUntil: '3 days',
+      productGroup: 'Dairy',
+    },
+  ];
+  
+  
+  const [products, setProducts] = useState(initialProducts);
+  
+  
+  function newProduct() {
+    const newProduct = {
+      name: 'Test Product',
+      daysUntil: '7 days',
+      productGroup: 'Group Test',
+    };
+  
+  
+    setProducts([...products, newProduct]);
+  }
+  
+  
     return (
       <div className="flex justify-center items-center">
       <div>
       <h1 className='mx-auto text-center'>Product List</h1>
+      <div className="text-center"> {/* Center the button */}
+          <button onClick={newProduct}>Add Product</button>
+        </div>
         {products.map((product, index) => (
           <Product
             key={index}
