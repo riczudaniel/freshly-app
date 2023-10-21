3c7e1f02d03eb70048fba55eaad57e770cc61643
riczudaniel <you@example.com>
2023-10-21 18:53:10 +0200

    Dinamic list rendering with bg

--- src/Components/ListTypes.js
+++ src/Components/ListTypes.js
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
-import freezer from "../images/freezer.png"
-import fridge from "../images/fridge.png"
-import pantry from "../images/pantry.png"
+import freezer from "../images/freezer.png";
+import fridge from "../images/fridge.png";
+import pantry from "../images/pantry.png";
 
 function ListTypes({ handleTypeSelect }) {
-    return (
-      <div className='flex my-4 justify-center'>
-        {[
-          { name: 'Freezer', image: freezer },
-          { name: 'Fridge', image: fridge },
-          { name: 'Pantry', image: pantry },
-        ].map((type, index) => (
-          <img
-            key={index}
-            src={type.image}
-            className={`w-28 mx-2 p-2 rounded-md`}
-            onClick={() => handleTypeSelect(type.name)}
-            alt={type.name}
-          />
-        ))}
-      </div>
-    );
-  }
+  const [selectedType, setSelectedType] = useState(null);
 
-export default ListTypes
+  return (
+    <div className='flex my-4 justify-center'>
+      {[
+        { name: 'Freezer', image: freezer },
+        { name: 'Fridge', image: fridge },
+        { name: 'Pantry', image: pantry },
+      ].map((type, index) => (
+        <img
+          key={index}
+          src={type.image}
+          className={`w-28 mx-2 p-2 rounded-md ${selectedType === type.name ? 'bg-gray-300' : ''}`}
+          onClick={() => {
+            handleTypeSelect(type.name);
+            setSelectedType(type.name);
+          }}
+          alt={type.name}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ListTypes;
